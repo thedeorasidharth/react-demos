f27a4d796239ff8589cf030df6dd581e755ba83e
thedeorasidharth <deorasidharth@gmail.com>
2025-08-28 12:21:34 +0530

    added something in about page

--- src/About.jsx
+++ src/About.jsx
@@ -5,6 +5,22 @@ function About() {
     <div style={{ padding: '20px', textAlign: 'center' }}>
       <h2>About Us</h2>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et officiis nemo qui error, dignissimos facilis accusamus molestiae quisquam, tempore asperiores nulla tenetur nam magni nostrum corrupti consequatur ex officia ut! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio hic, optio labore molestiae a nihil reprehenderit cum itaque corporis eum quod cupiditate nesciunt illum incidunt id blanditiis qui eveniet minus.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et officiis nemo qui error, dignissimos facilis accusamus molestiae quisquam, tempore asperiores nulla tenetur nam magni nostrum corrupti consequatur ex officia ut! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio hic, optio labore molestiae a nihil reprehenderit cum itaque corporis eum quod cupiditate nesciunt illum incidunt id blanditiis qui eveniet minus.</p>
+      <br />
+       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni culpa unde ullam exercitationem perferendis ad at? Distinctio et, eligendi reprehenderit sapiente illo necessitatibus numquam dignissimos repellat, eius molestias iusto quia!</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et officiis nemo qui error, dignissimos facilis accusamus molestiae quisquam, tempore asperiores nulla tenetur nam magni nostrum corrupti consequatur ex officia ut! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio hic, optio labore molestiae a nihil reprehenderit cum itaque corporis eum quod cupiditate nesciunt illum incidunt id blanditiis qui eveniet minus.</p>
+       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni culpa unde ullam exercitationem perferendis ad at? Distinctio et, eligendi reprehenderit sapiente illo necessitatibus numquam dignissimos repellat, eius molestias iusto quia!</p>
+       <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et officiis nemo qui error, dignissimos facilis accusamus molestiae quisquam, tempore asperiores nulla tenetur nam magni nostrum corrupti consequatur ex officia ut! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio hic, optio labore molestiae a nihil reprehenderit cum itaque corporis eum quod cupiditate nesciunt illum incidunt id blanditiis qui eveniet minus.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni culpa unde ullam exercitationem perferendis ad at? Distinctio et, eligendi reprehenderit sapiente illo necessitatibus numquam dignissimos repellat, eius molestias iusto quia!</p>
+       <br />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et officiis nemo qui error, dignissimos facilis accusamus molestiae quisquam, tempore asperiores nulla tenetur nam magni nostrum corrupti consequatur ex officia ut! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio hic, optio labore molestiae a nihil reprehenderit cum itaque corporis eum quod cupiditate nesciunt illum incidunt id blanditiis qui eveniet minus.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni culpa unde ullam exercitationem perferendis ad at? Distinctio et, eligendi reprehenderit sapiente illo necessitatibus numquam dignissimos repellat, eius molestias iusto quia!
+      </p>
+      <br />
+      <br />
+      <h4>Thanks for visiting</h4>
+      <br />
     </div>
   );
 }
